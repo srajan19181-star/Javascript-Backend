169c8b825ee46af621d196e56afc8850023c06a3
srajan19181-star <srajan19181@gmail.com>
2026-04-04 19:50:16 +0530

    user and video model

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,7 +1,10 @@
-class Apiresponse {
+class ApiResponse {
     constructor(statusCode,message = "Success",data = null){
-        this.statusCode = statusCode
-        this.message = message
-        this.data = data
-        this.success = statusCode
-}
+        this.statusCode = statusCode,
+        this.message = message,
+        this.data = data,
+        this.success = statusCode < 400
+    }
+};
+
+export {ApiResponse};
